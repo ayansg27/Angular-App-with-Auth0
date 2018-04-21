c556de0606058b7a9dbdad7da516ca308ad5bd1e
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-04-21 18:03:08 -0400

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {routing, appRoutingProviders} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule],
+  declarations: [ AppComponent,  HomeComponent, ProfileComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+      appRoutingProviders
+  ]
 })
 export class AppModule { }
